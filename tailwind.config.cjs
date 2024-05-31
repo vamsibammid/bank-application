749ec8c78b6d022c9f0394c02459775db830a2d6
User Krishna <vamsiprogton@gmail.com>
2024-05-31 17:29:21 +0530

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Path to your index.html file
+    './src/**/*.{js,jsx,ts,tsx}', // Path to your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
